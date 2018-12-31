0942f633db806e13f15d63bc2d8e0403f7f1386a
macd2 <waypointlabs@gmail.com>
2018-12-31 16:36:18 +0100

    removing unnecessary charackters
    solving the issue described here:
    https://github.com/shirosaidev/stocksight/issues/2

--- sentiment.py
+++ sentiment.py
@@ -763,7 +763,12 @@ if __name__ == '__main__':
             try:
                 f = open(twitter_users_file, "w")
                 for i in useridlist:
-                    f.write(str(i) + '\n')
+                    try:
+                        i = str(i).replace('b','')
+                        i = i.replace("'","")     
+                    except Exception:
+                        pass
+                    f.write(i + '\n')
                 f.close()
             except (IOError, OSError) as e:
                 logger.warning("Exception: error writing to file caused by: %s" % e)
